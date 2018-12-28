ab6de814f703e12dc90a373b1335e016e13f25ae
Steven Nguyen <nguyens5@uci.edu>
2018-12-28 00:55:17 -0800

    Installed axios + setup basic api config util

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,28 +1,6 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+const axios = require("axios");
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save sto reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
-    }
-}
-
-export default App;
+const instance = axios.create({
+    baseURL: "http://localhost:3000",
+    timeout: 60000
+});
