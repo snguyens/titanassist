731c99e4250a8f50e2f69fe2e13d80c32d690cc2
Steven Nguyen <nguyens5@uci.edu>
2020-02-27 00:25:03 -0800

    Update more js files to ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,33 +1,33 @@
 import { getClassSections } from "../services";
 import { CLASS_SECTIONS } from "../constants/display";
 
-export const updateClassSections = classSections => ({
+export const updateClassSections = (classSections: any) => ({
   type: "UPDATE_CLASS_SECTIONS",
   classSections
 });
 
-export const updateDisplay = display => ({
+export const updateDisplay = (display: any) => ({
   type: "UPDATE_DISPLAY",
   display
 });
 
-export const addClass = details => ({
+export const addClass = (details: any) => ({
   type: "ADD_CLASS",
   class: details
 });
 
-export const removeClass = code => ({
+export const removeClass = (code: any) => ({
   type: "REMOVE_CLASS",
   code
 });
 
-export const updateClassNumber = classNumber => ({
+export const updateClassNumber = (classNumber: any) => ({
   type: "UPDATE_CLASS_NUMBER",
   classNumber
 });
 
-export const searchForClasses = configs => {
-  return async function(dispatch) {
+export const searchForClasses = (configs: any) => {
+  return async function(dispatch: any) {
     try {
       const classSections = await getClassSections(configs);
       dispatch(updateClassSections(classSections));
